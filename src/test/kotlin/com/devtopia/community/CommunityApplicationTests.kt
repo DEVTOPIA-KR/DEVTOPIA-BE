544da897fb1fdcb787d49ecf7758dcc2e1d1f754
puleugo <puleugo@gmail.com>
2024-06-12 22:59:31 +0900

    feat: 형상관리 구현
    - application.properties 환경에 따라 분리. (prod, local)
    - 애플리케이션 실행 시 ActiveProfile 검증 로직 추가

--- src/test/kotlin/com/devtopia/community/CommunityApplicationTests.kt
+++ src/test/kotlin/com/devtopia/community/CommunityApplicationTests.kt
@@ -2,7 +2,9 @@ package com.devtopia.community
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("local", "prod")
 class CommunityApplicationTests() {}
