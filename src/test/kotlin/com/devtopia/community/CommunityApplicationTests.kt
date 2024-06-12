4a3b5ff17d3fd53c20401ad9d07a64caedcb9d12
puleugo <puleugo@gmail.com>
2024-06-12 23:03:24 +0900

    refactor: SpringBootTest 옵션 변경
    mockMvc를 통해 테스트로하므로 RANDOM_PORT보다 MOCK 옵션으로 변경했습니다.

--- src/test/kotlin/com/devtopia/community/CommunityApplicationTests.kt
+++ src/test/kotlin/com/devtopia/community/CommunityApplicationTests.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest()
 @AutoConfigureMockMvc
 @ActiveProfiles("local", "prod")
 class CommunityApplicationTests() {}
